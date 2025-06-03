b4ec505bd85bb1724914b4fcccd19938767a7755
trontium <2909858946@qq.com>
2025-06-03 19:24:13 +0800

    11

---
+++ vue-project/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>
+    你好啊
+  </h1>
+  </div>
+</template>
+
+<script lang="ts">
+  export default {
+    name: 'App',
+  
+  }
+
+</script>
+
+<style>
+
+.app{
+  background-color: #ddd;
+  box-shadow:0 0 10px ;
+  border-radius: 10px;
+  padding: 20px;
+}
+</style>

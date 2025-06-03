c6ef01c86e35ad3f9db3467e703308a2b48999b3
trontium <2909858946@qq.com>
2025-06-03 19:39:03 +0800

    1

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,19 +1,15 @@
-<template>
-  <h1 :class='titleClass'> Make me red</h1>
-</template>
-
 <script setup>
-  import { ref } from 'vue'
-  const titleClass =ref('title')
+import { ref } from 'vue'
 
+const titleClass = ref('title')
 </script>
 
-<style>
+<template>
+  <h1 :class="titleClass">Make me red</h1>
+</template>
 
-.app{
-  background-color: #ddd;
-  box-shadow:0 0 10px ;
-  border-radius: 10px;
-  padding: 20px;
+<style>
+.title {
+  color: red;
 }
 </style>

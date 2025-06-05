af8cb0a129641e4e4f7c86a920debcd97ebde546
trontium <2909858946@qq.com>
2025-06-05 20:24:46 +0800

    Layout

--- vue3-rabbit/src/views/Layout/index.vue
+++ vue3-rabbit/src/views/Layout/index.vue
@@ -1,5 +1,13 @@
+<script setup>
+import LayoutHeader from './components/LayoutHeader.vue'
+import LayoutFooter from './components/LayoutFooter.vue'
+import LayoutNav from './components/LayoutNav.vue'
+</script>
+
 <template>
-  <div>我是首页
-    <RouterView />
-  </div>
+  <LayoutHeader />
+  <LayoutNav />
+  <RouterView />
+  <LayoutFooter />
+
 </template>
